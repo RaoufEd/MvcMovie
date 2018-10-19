2183358a5cb1fab3cc4b0c84707876e5937e4d77
RaoufEd <raouf.edabbar@gmail.com>
2018-10-19 20:08:01 +0100

    tests routes

---
+++ MvcMovie/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /Hello/
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+        //// GET: /Hello/Welcome/
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+        // GET: /Hello/Welcome/
+        [Route("WelcomeYou/{name}/{numTimes:int}")]
+        public string Welcome(string name, int id)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + id);
+        }
+
+    }
+}
